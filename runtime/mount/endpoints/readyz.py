eb955585f9615d8ab855b7810f4690ae14f8b814
Ryan Teoh <ryan@latch.bio>
2024-07-30 10:22:05 -0700

    readyz telemetry

--- runtime/mount/endpoints/readyz.py
+++ runtime/mount/endpoints/readyz.py
@@ -1,11 +1,15 @@
-from latch_asgi.context.common import Context
-from latch_asgi.context.http import HandlerResult
+from latch_asgi.context.http import Context, HandlerResult
 from latch_asgi.framework.http import HTTPInternalServerError
+from latch_o11y.o11y import trace_function_with_span
+from opentelemetry.trace import Span, get_tracer
 
 from ..entrypoint import k_proc, start_kernel_proc
 
+tracer = get_tracer(__name__)
 
-async def readyz(ctx: Context) -> HandlerResult:
+
+@trace_function_with_span(tracer)
+async def readyz(s: Span, ctx: Context) -> HandlerResult:
     proc = k_proc.proc
     if proc is None:
         raise HTTPInternalServerError("Kernel process not started")
