34f760fe8a62cc6e2a538f0aaafadd964967508b
manske <nathan@latch.bio>
2024-08-23 12:32:32 -0700

    update row key assertion

--- runtime/mount/python_lib/lplots/widgets/row.py
+++ runtime/mount/python_lib/lplots/widgets/row.py
@@ -6,6 +6,7 @@ from . import _emit, _state
 class RowWidgetState(_emit.WidgetState[Literal["row"], None]):
     items: list[str]
 
+
 def w_row(
     *,
     key: str | None = None,
@@ -14,13 +15,12 @@ def w_row(
     key = _state.use_state_key(key=key)
 
     # todo(manske): have better type checking here for widgets
-    assert all(hasattr(i, "key") for i in items)
-    item_keys = [i.key for i in items]
+    assert all(hasattr(i, "_key") for i in items)
+    item_keys = [i._key for i in items]
 
     w: RowWidgetState = {
         "type": "row",
         "items": set(item_keys),
     }
-
 
     _emit.emit_widget(key, w)
