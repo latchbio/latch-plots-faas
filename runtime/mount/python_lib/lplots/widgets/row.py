f4c0f781bf4ebb900feae814f4ce61c8fbbb00e9
manske <nathan@latch.bio>
2024-08-23 12:09:22 -0700

    Add row widget to plots

---
+++ runtime/mount/python_lib/lplots/widgets/row.py
@@ -0,0 +1,26 @@
+from typing import Literal
+
+from . import _emit, _state
+
+
+class RowWidgetState(_emit.WidgetState[Literal["row"], None]):
+    items: list[str]
+
+def w_row(
+    *,
+    key: str | None = None,
+    items: set[object],
+) -> None:
+    key = _state.use_state_key(key=key)
+
+    # todo(manske): have better type checking here for widgets
+    assert all(hasattr(i, "key") for i in items)
+    item_keys = [i.key for i in items]
+
+    w: RowWidgetState = {
+        "type": "row",
+        "items": set(item_keys),
+    }
+
+
+    _emit.emit_widget(key, w)
