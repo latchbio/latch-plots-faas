da8197e07073adafaa05a84d5adb0df8c886d076
manske <nathan@latch.bio>
2024-08-25 22:26:08 -0700

    pr fixes

--- runtime/mount/python_lib/lplots/widgets/row.py
+++ runtime/mount/python_lib/lplots/widgets/row.py
@@ -1,6 +1,25 @@
 from typing import Literal
 
+from runtime.mount.lplots.widgets.checkbox import CheckboxWidget
+from runtime.mount.lplots.widgets.dataframe import DataframePicker
+from runtime.mount.lplots.widgets.ldata import LDataPicker
+from runtime.mount.lplots.widgets.multiselect import MultiSelect
+from runtime.mount.lplots.widgets.radio import RadioGroups
+from runtime.mount.lplots.widgets.registry import RegistryTablePicker
+from runtime.mount.lplots.widgets.select import Select
+
 from . import _emit, _state
+
+AllowedWidgets = (
+    CheckboxWidget
+    | LDataPicker
+    | Select
+    | RadioGroups
+    | MultiSelect
+    | CheckboxWidget
+    | DataframePicker
+    | RegistryTablePicker
+)
 
 
 class RowWidgetState(_emit.WidgetState[Literal["row"], None]):
@@ -10,17 +29,13 @@ class RowWidgetState(_emit.WidgetState[Literal["row"], None]):
 def w_row(
     *,
     key: str | None = None,
-    items: set[object],
+    items: set[AllowedWidgets],
 ) -> None:
     key = _state.use_state_key(key=key)
 
-    # todo(manske): have better type checking here for widgets
-    assert all(hasattr(i, "_key") for i in items)
-    item_keys = [i._key for i in items]
-
     w: RowWidgetState = {
         "type": "row",
-        "items": list(item_keys),
+        "items": [i._key for i in items],
     }
 
     _emit.emit_widget(key, w)
