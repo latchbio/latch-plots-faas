c439b71729a262620a4868aa1f8395ef766675de
Ryan Teoh <ryan@latch.bio>
2024-10-17 09:17:48 -0700

    Revert "allow any objects as options to select/multiselect"
    This reverts commit 82908d3551b805c8bf4859774db084e7689ec0a9.

--- runtime/mount/python_lib/lplots/widgets/multiselect.py
+++ runtime/mount/python_lib/lplots/widgets/multiselect.py
@@ -1,6 +1,6 @@
 from collections.abc import Iterable
 from dataclasses import dataclass
-from typing import Any, Literal, NotRequired
+from typing import Literal, NotRequired
 
 from ..reactive import Signal
 from . import _emit, _state
@@ -11,7 +11,6 @@ class MultiSelectState(_emit.WidgetState[Literal["multi_select"], str]):
     label: str
     readonly: bool
     options: list[str]
-    option_values: dict[str, Any]
     default: NotRequired[list[str] | None]
     appearance: NotRequired[FormInputAppearance | None]
     required: bool
@@ -24,21 +23,18 @@ class MultiSelect:
     _signal: Signal[list[str]]
 
     @property
-    def value(self) -> list[Any] | None:
+    def value(self) -> list[str] | None:
         res = self._signal()
-        option_values = self._state["option_values"]
-
         if (
             res is None
             or not isinstance(res, list)
             or not all(isinstance(x, str) for x in res)
-            or not all(x in option_values for x in res)
         ):
             res = self._state.get("default")
             if res is None:
                 return None
 
-        return [option_values[x] for x in res if x in option_values]
+        return [x for x in res if x in self._state["options"]]
 
 
 def w_multi_select(
@@ -47,21 +43,11 @@ def w_multi_select(
     label: str,
     readonly: bool = False,
     appearance: FormInputAppearance | None = None,
-    options: Iterable[Any],
-    default: Iterable[Any] | None = None,
+    options: Iterable[str],
+    default: list[str] | None = None,
     required: bool = False,
 ) -> MultiSelect:
     key = _state.use_state_key(key=key)
-
-    option_values: dict[str, Any] = {}
-
-    for opt in options:
-        assert hasattr(opt, "__str__")
-        option_values[str(opt)] = opt
-
-    if default is not None:
-        for val in default:
-            assert hasattr(val, "__str__")
 
     res = MultiSelect(
         _key=key,
@@ -69,9 +55,8 @@ def w_multi_select(
             "type": "multi_select",
             "label": label,
             "readonly": readonly,
-            "options": [str(opt) for opt in options],
-            "option_values": option_values,
-            "default": [str(val) for val in default] if default is not None else None,
+            "options": list(options),
+            "default": default,
             "appearance": appearance,
             "required": required,
         },
--- runtime/mount/python_lib/lplots/widgets/select.py
+++ runtime/mount/python_lib/lplots/widgets/select.py
@@ -1,6 +1,6 @@
 from collections.abc import Iterable
 from dataclasses import dataclass
-from typing import Any, Literal, NotRequired
+from typing import Literal, NotRequired
 
 from ..reactive import Signal
 from . import _emit, _state
@@ -11,7 +11,6 @@ class SelectState(_emit.WidgetState[Literal["select"], str]):
     label: str
     readonly: bool
     options: list[str]
-    option_values: dict[str, Any]
     default: NotRequired[str | None]
     appearance: NotRequired[FormInputAppearance | None]
     required: bool
@@ -24,16 +23,15 @@ class Select:
     _signal: Signal[str]
 
     @property
-    def value(self) -> Any | None:
+    def value(self) -> str | None:
         res = self._signal()
 
-        option_values = self._state["option_values"]
-        if res is None or not isinstance(res, str) or res not in option_values:
+        if res is None or not isinstance(res, str) or res not in self._state["options"]:
             res = self._state.get("default")
             if res is None:
                 return None
 
-        return option_values[res]
+        return res
 
 
 def w_select(
@@ -41,20 +39,12 @@ def w_select(
     key: str | None = None,
     label: str,
     readonly: bool = False,
-    options: Iterable[Any],
-    default: Any | None = None,
+    options: Iterable[str],
+    default: str | None = None,
     appearance: FormInputAppearance | None = None,
     required: bool = False,
 ) -> Select:
     key = _state.use_state_key(key=key)
-
-    option_values: dict[str, Any] = {}
-    for opt in options:
-        assert hasattr(opt, "__str__")
-        option_values[str(opt)] = opt
-
-    if default is not None:
-        assert hasattr(default, "__str__")
 
     res = Select(
         _key=key,
@@ -62,9 +52,8 @@ def w_select(
             "type": "select",
             "label": label,
             "readonly": readonly,
-            "options": [str(opt) for opt in options],
-            "option_values": option_values,
-            "default": str(default) if default is not None else None,
+            "options": list(options),
+            "default": default,
             "appearance": appearance,
             "required": required,
         },
