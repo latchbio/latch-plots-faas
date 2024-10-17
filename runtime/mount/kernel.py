d7d2cc01ccb7711533a58313a6bfb230ab1b6cf4
maximsmol <1472826+maximsmol@users.noreply.github.com>
2024-10-17 12:07:22 -0700

    fix
    Signed-off-by: maximsmol <1472826+maximsmol@users.noreply.github.com>

--- runtime/mount/kernel.py
+++ runtime/mount/kernel.py
@@ -30,7 +30,7 @@ from pandas.io.json._table_schema import build_table_schema
 from plotly.basedatatypes import BaseFigure
 from plotly_utils.precalc_box import precalc_box
 from plotly_utils.precalc_violin import precalc_violin
-from .socketio_thread import SocketIoThread
+from socketio_thread import SocketIoThread
 from stdio_over_socket import SocketWriter, text_socket_writer
 
 sys.path.append(str(Path(__file__).parent.absolute()))
