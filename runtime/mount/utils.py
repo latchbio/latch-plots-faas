04b7de679442c9b2e1dd87f40723f26edeca4339
Ryan Teoh <ryan@latch.bio>
2024-10-31 16:27:33 -0700

    revert

--- runtime/mount/utils.py
+++ runtime/mount/utils.py
@@ -128,6 +128,6 @@ def orjson_encoder(obj: Any) -> Any:
         return obj.remote_path
 
     if isinstance(obj, Enum):
-        return obj.value
+        return obj.name
 
     raise TypeError(f"Type {type(obj)} not serializable")
