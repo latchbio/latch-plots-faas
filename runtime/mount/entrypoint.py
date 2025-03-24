ac1bd2a4c9a60c055ca0938dee7560a0a82935c0
Ryan Teoh <ryan@latch.bio>
2025-03-24 11:57:03 -0700

    set active_cell

--- runtime/mount/entrypoint.py
+++ runtime/mount/entrypoint.py
@@ -143,8 +143,9 @@ async def handle_kernel_messages(conn_k: SocketIo, auth: str) -> None:
 
             elif msg["type"] == "start_cell":
                 cell_id = msg["cell_id"]
+                active_cell = cell_id
+
                 if cell_id is not None:
-                    active_cell = cell_id
                     cell_sequencers[cell_id] = msg["run_sequencer"]
                     cell_status[cell_id] = "running"
 
