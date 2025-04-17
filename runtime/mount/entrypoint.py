7ff65348a2cd50c566ad9896e475f9b4c75224ea
AidanAbd <aidan@latch.bio>
2025-04-17 11:45:07 -0700

    fix: some fresh mechanisms

--- runtime/mount/entrypoint.py
+++ runtime/mount/entrypoint.py
@@ -369,6 +369,7 @@ async def handle_kernel_messages(conn_k: SocketIo, auth: str) -> None:
                         }
                     }
                 )
+                return
 
             elif msg["type"] == "plot_data":
                 await gql_query(
