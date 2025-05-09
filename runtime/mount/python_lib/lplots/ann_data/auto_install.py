b31be45af85d2a6cd93f2bec036aa3000e80a03c
Kenny Workman <kennyworkman@sbcglobal.net>
2025-05-09 12:42:26 -0700

    dumb

--- runtime/mount/python_lib/lplots/ann_data/auto_install.py
+++ runtime/mount/python_lib/lplots/ann_data/auto_install.py
@@ -24,8 +24,8 @@ ad = install_and_import_anndata()
 
 def install_and_import_stalign_and_torch() -> tuple["STalign", "torch"]:  # type: ignore
     try:
-        import STalign  # type: ignore  # noqa: PLC0415
         import torch  # type: ignore  # noqa: PLC0415
+        from STalign import STalign  # type: ignore  # noqa: PLC0415
     except ImportError:
 
         # note(kenny): author's dumped pip freeze into build requirements in
@@ -35,7 +35,7 @@ def install_and_import_stalign_and_torch() -> tuple["STalign", "torch"]:  # type
             "-m", "pip", "install", "--no-deps",
             "git+https://github.com/JEFworks-Lab/STalign.git@b2068edc98974efa54537eca194736e177bbe11d"
         ])
-        import STalign  # type: ignore  # noqa: PLC0415
+        from STalign import STalign  # type: ignore  # noqa: PLC0415
         # note(kenny): need strict installation order and dependency resolution
         # from a single pip install does not provide this guarantee
         subprocess.check_call([sys.executable, "-m", "pip", "install", "torch===2.7.0"])
