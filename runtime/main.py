662432b14bed1bd2f5f9bc2142fde6daa1d102a5
AidanAbd <aidan@latch.bio>
2025-08-16 14:14:28 -0700

    fix: try uvcorn

--- runtime/main.py
+++ runtime/main.py
@@ -5,20 +5,14 @@ from latch_o11y.o11y import setup as setup_o11y
 setup_o11y()
 
 import asyncio
+import signal
 
-from hypercorn.asyncio import serve
-from hypercorn.config import Config as HypercornConfig
+import uvicorn
 from latch_asgi.server import LatchASGIServer
 
 from .config import config
 from .mount.endpoints import http_routes, websocket_routes
 from .mount.entrypoint import shutdown, start_kernel_proc
-
-cfg = HypercornConfig()
-cfg.bind = ["[::]:5000"]
-cfg.use_reloader = config.auto_reload
-cfg.graceful_timeout = 0.1
-
 
 latch_server = LatchASGIServer(
     http_routes=http_routes,
@@ -29,6 +23,34 @@ latch_server = LatchASGIServer(
 
 app = latch_server.raw_app
 
+
+async def _run_uvicorn() -> None:
+    uv_config = uvicorn.Config(
+        app,
+        host="0.0.0.0",
+        port=5000,
+        ws="websockets",
+        reload=config.auto_reload,
+        log_config=None,
+    )
+    server = uvicorn.Server(uv_config)
+
+    shutdown_event = asyncio.Event()
+
+    def shutdown_signal(*_args: object) -> None:
+        shutdown_event.set()
+
+    loop = asyncio.get_running_loop()
+    loop.add_signal_handler(signal.SIGTERM, shutdown_signal)
+    loop.add_signal_handler(signal.SIGINT, shutdown_signal)
+
+    async def _shutdown_watcher() -> None:
+        await shutdown_event.wait()
+        server.should_exit = True
+
+    await asyncio.gather(server.serve(), _shutdown_watcher())
+
+
 if __name__ == "__main__":
     if sys.platform == "linux":
         from ctypes import CDLL
@@ -37,21 +59,7 @@ if __name__ == "__main__":
         PR_SET_NAME = 15  # https://github.com/torvalds/linux/blob/2df0c02dab829dd89360d98a8a1abaa026ef5798/include/uapi/linux/prctl.h#L56
         libc.prctl(PR_SET_NAME, b"latch-plots")
 
-    import signal
-
     import uvloop
 
     uvloop.install()
-    loop = asyncio.new_event_loop()
-
-    shutdown_event = asyncio.Event()
-
-    async def await_shutdown() -> None:
-        await shutdown_event.wait()
-
-    def shutdown_signal(*args) -> None:
-        shutdown_event.set()
-
-    loop.add_signal_handler(signal.SIGTERM, shutdown_signal)
-    loop.add_signal_handler(signal.SIGINT, shutdown_signal)
-    loop.run_until_complete(serve(app, cfg, shutdown_trigger=await_shutdown))
+    asyncio.run(_run_uvicorn())
