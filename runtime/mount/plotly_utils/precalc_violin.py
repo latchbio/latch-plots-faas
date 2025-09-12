a5f1dff23d88e871e69507354d8fbf274a3ceeb7
Tim Proctor <28866303+t-proctor@users.noreply.github.com>
2025-09-11 21:48:57 -0700

    test w/ proper data

--- runtime/mount/plotly_utils/precalc_violin.py
+++ runtime/mount/plotly_utils/precalc_violin.py
@@ -119,7 +119,7 @@ def precalc_violin(trace: Any):
         trace["maxKDE"].append(maxKDE)
 
     # add dummy val to make trace shown
-    trace[data_axis] = []
+    trace[data_axis] = trace_data
     # import random
     # import random
     # trace[index_axis] = np.asarray([random.randint(-100, 0)])
