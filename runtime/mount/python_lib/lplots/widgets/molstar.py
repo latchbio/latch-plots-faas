3f182800870905466261301e893b73fc70f700d2
manske <nathan@latch.bio>
2025-11-21 10:01:46 -0800

    add molstar stuffs

---
+++ runtime/mount/python_lib/lplots/widgets/molstar.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+from typing import Literal, NotRequired, TypedDict
+
+from molviewspec.builder import Root, State
+
+from . import _emit, _state, widget
+
+molstar_type: Literal["molstar"] = "molstar"
+
+
+class MolstarOptions(TypedDict, total=False):
+    """Options for Molstar widget configuration."""
+
+
+class MolstarState(_emit.WidgetState[molstar_type, str]):
+    label: NotRequired[str | None]
+    options: MolstarOptions
+    molstarviewspec: NotRequired[State | None]
+
+
+@dataclass(frozen=True, kw_only=True)
+class Molstar(widget.BaseWidget):
+    _key: str
+    _state: MolstarState
+
+
+_emit.widget_registry[molstar_type] = Molstar
+
+
+def w_molstar(
+    *, key: str | None = None, label: str | None = None, options: MolstarOptions, molstarviewspec_builder: Root | None = None
+) -> Molstar:
+    key = _state.use_state_key(key=key)
+
+    if molstarviewspec_builder is not None:
+        molstarviewspec = molstarviewspec_builder.get_state()
+    else:
+        molstarviewspec = None
+
+    res = Molstar(
+        _key=key,
+        _state={
+            "type": molstar_type,
+            "options": options,
+            "label": label,
+            "molstarviewspec": molstarviewspec,
+        },
+    )
+    _emit.emit_widget(key, res._state)
+
+    return res
