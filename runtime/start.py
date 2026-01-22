b20f4eedc675dfe4211c0967da837d401b8c7718
Tim Proctor <28866303+t-proctor@users.noreply.github.com>
2026-01-22 12:11:52 -0800

    rem q

--- runtime/start.py
+++ runtime/start.py
@@ -51,7 +51,7 @@ os.system("/opt/mamba/envs/plots-faas/bin/pip install --upgrade latch")
 
 technology_docs = Path("/opt/latch/plots-faas/runtime/mount/agent_config/context/technology_docs")
 for sp_requirements in technology_docs.glob("*/requirements.txt"):
-    os.system(f"/opt/mamba/envs/plots-faas/bin/pip install -q --no-deps -r {sp_requirements}")
+    os.system(f"/opt/mamba/envs/plots-faas/bin/pip install --no-deps -r {sp_requirements}")
 
 os.execle(
     "/usr/bin/nice",
