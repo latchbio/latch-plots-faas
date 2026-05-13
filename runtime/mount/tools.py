eef93f7662624b0a0ffd79e7c5502500010507e5
Ryan Teoh <ryan@latch.bio>
2026-05-13 16:01:47 -0700

    fix: imports

--- runtime/mount/tools.py
+++ runtime/mount/tools.py
@@ -14,9 +14,8 @@ from claude_agent_sdk import (
 from latch_data_validation.data_validation import validate
 from loro import LoroMap
 from lplots import _inject
-
-from runtime.mount.notebook.notebook import get_notebook, parse_ts_container_id
-from runtime.mount.utils import auth_token_sdk, gql_query
+from notebook.notebook import get_notebook, parse_ts_container_id
+from utils import auth_token_sdk, gql_query
 
 
 @dataclass(frozen=True)
@@ -39,6 +38,7 @@ class RedeemPackageData:
 @dataclass(frozen=True)
 class RedeemPackageRes:
     data: RedeemPackageData
+
 
 if TYPE_CHECKING:
     from agent import AgentHarness
